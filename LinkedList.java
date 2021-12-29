3dd235fbb3fa783de55809d714a6b95b1425cf3d
Terrence <tkbthomas2192@gmail.com>
2021-12-29 16:18:22 -0500

    successful code test

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,21 @@
+import java.io.*;
+import java.util.*;
 
-public class LinkedListClass{
-    
-    public static void main(String[] args){
-
+public class Solution {
+    // Node Struct
+    static class Node{
+        int data;
+        Node next;
+        // Constrcutor initalizes the data value
+        Node(int d){data=d;next=null;}
     }
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+        LinkedList<Node> list = new LinkedList<Node>();
+        while(scanner.hasNext()){
+            list.addFirst(new Node(scanner.nextInt()));
+        }
+        list.forEach((node)->{System.out.println(node.data);});
+    }
 }
